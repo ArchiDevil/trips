3e6ad30151209c18538e44509b39798570b75c0a
Denis Bezykornov <archidevil52@gmail.com>
2019-08-05 18:51:23 +0000

    Merged in r1_trips_screen (pull request #7)
    R1 trips screen
    
    * Add trip page:
        Moment.js integration
        Initial implementation of add page
        Heavy DB update
        Fake users table with fake passwords now
    
    * Fix a typo, return back DATE type to DB for trips
    
    * Add form validation, title for the page
    
    * 'Days' page for trip:
        * Editing trip
        * Brief trip info
        * Stub for the table
    
    * Add trip archivation mechanism
    
    * Update version to 0.2 rc1
        Attempt to make fluid markup in trips screen
    
    * Attempt to make fluid markup in trips screen
    
        Change version to 0.2

--- organizer/schema.sql
+++ organizer/schema.sql
@@ -2,35 +2,49 @@ DROP TABLE IF EXISTS trips;
 
 CREATE TABLE trips (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    from_date DATE,
-    till_date DATE
+    name TEXT NOT NULL,
+    from_date DATE NOT NULL,
+    till_date DATE NOT NULL,
+    attendees INTEGER NOT NULL,
+    last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    archived INTEGER NOT NULL DEFAULT 0
 );
 
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    calories REAL,
-    proteins REAL,
-    fats REAL,
-    carbs REAL,
+    name TEXT NOT NULL,
+    calories REAL NOT NULL,
+    proteins REAL NOT NULL,
+    fats REAL NOT NULL,
+    carbs REAL NOT NULL,
     archived INTEGER DEFAULT 0
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    access_group INTEGER NOT NULL
 );
 
 --- fake data
 
-INSERT INTO trips(name, from_date, till_date) VALUES (
-    "First card",
+INSERT INTO trips(name, from_date, till_date, attendees) VALUES (
+    "Mt. Everest trip",
     "2019-01-01",
-    "2019-01-09"
+    "2019-01-09",
+    5
 );
 
-INSERT INTO trips(name, from_date, till_date) VALUES (
-    "Second card",
+INSERT INTO trips(name, from_date, till_date, attendees) VALUES (
+    "Rocky Mountains",
     "2019-06-01",
-    "2019-07-09"
+    "2019-07-09",
+    3
 );
 
 ---
@@ -53,3 +67,21 @@ INSERT INTO products(name, calories, proteins, fats, carbs, archived) VALUES (
     2, 3, 3, 
     1
 );
+
+---
+
+--- password 'qwerty'
+INSERT INTO users(name, password, access_group)
+VALUES ("Administrator", "pbkdf2:sha256:150000$y8RuRagx$371e6520ae64c1c4c367adb82b076b081068333511ef3f2c3ccab0107494d5f0", 0);
+
+--- password 'org'
+INSERT INTO users(name, password, access_group)
+VALUES ("Organizer",     "pbkdf2:sha256:150000$3ngYsSXZ$4a693ab6cacc753ed1b18ce51757e6d9d85c25ed02c422ec66dae70b92ea11b2", 1);
+
+--- password 'user1'
+INSERT INTO users(name, password, access_group)
+VALUES ("User1",         "pbkdf2:sha256:150000$LXwcrYlk$45da8f74b50caf71fa6933de95bff3d959618c8098b2f0d45967cba2623cede3", 2);
+
+--- password 'user2'
+INSERT INTO users(name, password, access_group)
+VALUES ("User2",         "pbkdf2:sha256:150000$eR8YdWL9$1edf7f04de6f688c179a276dfac1416d0faf5ae583ab608617d8317846532f2b", 2);
--- organizer/trips.py
+++ organizer/trips.py
@@ -1,15 +1,88 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
 import flask
 
 from organizer.db import get_db
 
 bp = Blueprint('trips', __name__)
 
+
 @bp.route('/')
 def index():
     db = get_db()
     days = db.execute(
-        'SELECT name, from_date, till_date FROM trips'
+        'SELECT id, name, from_date, till_date, last_update FROM trips WHERE archived=0'
     ).fetchall()
 
-    return render_template('trips.j2', trip_days=days)
+    return render_template('trips/trips.j2', trip_days=days)
+
+
+@bp.route('/trips/add', methods=('GET', 'POST'))
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        daterange = request.form['daterange']
+        attendees = request.form['attendees']
+
+        from_date, till_date = daterange.split(' - ')
+
+        db = get_db()
+        db.execute(
+            'INSERT INTO trips(name, from_date, till_date, attendees) VALUES (?, ?, ?, ?)',
+            (name, from_date, till_date, attendees)
+        )
+        db.commit()
+
+        return redirect(url_for('trips.index'))
+
+    return render_template('trips/add.j2')
+
+
+@bp.route('/trips/trip/<int:trip_id>')
+def trip(trip_id):
+    db = get_db()
+
+    trip_info = db.execute(
+        'SELECT id, name, from_date, till_date FROM trips WHERE id=?',
+        (trip_id,)
+    ).fetchone()
+
+    return render_template('trips/days.j2', trip=trip_info)
+
+
+@bp.route('/trips/edit/<int:trip_id>', methods=('GET', 'POST'))
+def edit(trip_id):
+    db = get_db()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        daterange = request.form['daterange']
+        attendees = request.form['attendees']
+
+        from_date, till_date = daterange.split(' - ')
+
+        db.execute(
+            'UPDATE trips SET name=?, from_date=?, till_date=?, attendees=? WHERE id=?',
+            (name, from_date, till_date, attendees, trip_id)
+        )
+        db.commit()
+
+        return redirect(url_for('trips.trip', trip_id=trip_id))
+
+    trip_info = db.execute(
+        'SELECT id, name, from_date, till_date, attendees FROM trips WHERE id=?',
+        (trip_id,)
+    ).fetchone()
+
+    return render_template('trips/edit.j2', trip=trip_info, action='edit')
+
+
+@bp.route('/trips/archive/<int:trip_id>')
+def archive(trip_id):
+    db = get_db()
+    db.execute(
+        'UPDATE trips SET archived=1 WHERE id=?',
+        (trip_id,)
+    )
+    db.commit()
+
+    return redirect(url_for('trips.index'))
