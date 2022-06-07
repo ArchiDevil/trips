9998bc55ccfedb815cad3ed4bb753213593ef5dc
Denis Bezykornov <archidevil52@gmail.com>
2022-06-07 20:23:10 +0300

    Add lembic to sdist, update another pipeline

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='trips_organizer',
-    version='0.3',
+    version='0.8',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=open('requirements.txt', encoding='utf-8').readlines(),
 
     url='https://bitbucket.org/ArchiDevil/trips/',
     author='Denis Bezykornov',
