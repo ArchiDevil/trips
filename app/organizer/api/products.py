2031b94100b20dce87e68f9887289589e8f3ff92
Denis Bezykornov <archidevil52@gmail.com>
2023-05-01 18:29:31 +0300

    Move remaining product things to APIs

--- app/organizer/api/products.py
+++ app/organizer/api/products.py
@@ -1,13 +1,59 @@
+import math
 from typing import Final
 
-from flask import Blueprint, request, url_for
+from flask import Blueprint, request, url_for, abort
+from sentry_sdk import capture_exception
 
 from organizer.auth import api_login_required_group
 from organizer.db import get_session
 from organizer.schema import Product, Units, AccessGroup
+from organizer.strings import STRING_TABLE
 
 
 BP = Blueprint('products', __name__, url_prefix='/products')
+
+
+def check_input_data(json_data: dict):
+    name = str(json_data['name'])
+    if not name or len(name) > 100:
+        raise RuntimeError(STRING_TABLE['Products error incorrect name'])
+
+    try:
+        calories = float(json_data['calories'])
+        if calories < 0 or math.isnan(calories):
+            raise ValueError
+    except ValueError:
+        raise RuntimeError(STRING_TABLE['Products error incorrect calories'])
+
+    try:
+        proteins = float(json_data['proteins'])
+        if not (proteins >= 0 and proteins <= 100):
+            raise ValueError
+    except ValueError:
+        raise RuntimeError(STRING_TABLE['Products error incorrect proteins'])
+
+    try:
+        fats = float(json_data['fats'])
+        if not (fats >= 0 and fats <= 100):
+            raise ValueError
+    except ValueError:
+        raise RuntimeError(STRING_TABLE['Products error incorrect fats'])
+
+    try:
+        carbs = float(json_data['carbs'])
+        if not (carbs >= 0 and carbs <= 100):
+            raise ValueError
+    except ValueError:
+        raise RuntimeError(STRING_TABLE['Products error incorrect carbs'])
+
+    grams = None
+    if 'grams' in json_data:
+        try:
+            grams = float(json_data['grams'])
+            if grams < 0 or math.isnan(grams):
+                raise ValueError
+        except ValueError:
+            raise RuntimeError(STRING_TABLE['Products error incorrect grams'])
 
 
 @BP.get('/search')
@@ -20,13 +66,14 @@ def search():
     with get_session() as session:
         if search_request:
             search_pattern = f"%{search_request}%"
-            products_count = session.query(Product.id).filter(Product.name.ilike(search_pattern),
-                                                              Product.archived == False).count()
-            products = session.query(Product).filter(Product.name.ilike(search_pattern),
-                                                     Product.archived == False).order_by(Product.id).offset(page * products_per_page).limit(products_per_page).all()
+            products_count = session.query(Product.id).filter(Product.name.ilike(search_pattern))
+            products = session.query(Product).filter(Product.name.ilike(search_pattern))
         else:
-            products_count = session.query(Product.id).filter(Product.archived == False).count()
-            products = session.query(Product).filter(Product.archived == False).order_by(Product.id).offset(page * products_per_page).limit(products_per_page).all()
+            products_count = session.query(Product.id)
+            products = session.query(Product)
+
+        products_count = products_count.filter(Product.archived == False).count()
+        products = products.filter(Product.archived == False).order_by(Product.id).offset(page * products_per_page).limit(products_per_page).all()
 
         return {
             'page': page,
@@ -41,8 +88,8 @@ def search():
                     'fats': product.fats,
                     'carbs': product.carbs,
                     'grams': product.grams,
-                    'edit_link': url_for('products.edit', product_id=product.id),
-                    'archive_link': url_for('products.archive', product_id=product.id)
+                    'edit_link': url_for('api.products.edit', product_id=product.id),
+                    'archive_link': url_for('api.products.archive', product_id=product.id)
                 } for product in products
             ]
         }
@@ -69,3 +116,100 @@ def product_units():
             'result': True,
             'units': units
         }
+
+
+@BP.post('/add')
+@api_login_required_group(AccessGroup.User)
+def add():
+    data = request.json
+    if not data:
+        abort(400)
+
+    try:
+        check_input_data(data)
+    except RuntimeError as exc:
+        capture_exception(exc)
+        return {
+            'result': False,
+            'error': str(exc)
+        }
+
+    name = data['name']
+    calories = data['calories']
+    proteins = data['proteins']
+    fats = data['fats']
+    carbs = data['carbs']
+
+    grams = None
+    if 'grams' in data:
+        grams = data['grams']
+
+    with get_session() as session:
+        prod = Product(name=name, calories=calories,
+                       proteins=proteins, fats=fats,
+                       carbs=carbs, grams=grams)
+        session.add(prod)
+        session.commit()
+
+    return {
+        'result': True
+    }
+
+
+@BP.post('/<int:product_id>/edit')
+@api_login_required_group(AccessGroup.Administrator)
+def edit(product_id: int):
+    data = request.json
+    if not data:
+        abort(400)
+
+    try:
+        check_input_data(data)
+    except RuntimeError as exc:
+        capture_exception(exc)
+        return {
+            'result': False,
+            'error': str(exc)
+        }
+
+    name = data['name']
+    calories = data['calories']
+    proteins = data['proteins']
+    fats = data['fats']
+    carbs = data['carbs']
+
+    grams = None
+    if 'grams' in data:
+        grams = data['grams']
+
+    with get_session() as session:
+        prod = session.query(Product).filter(Product.id == product_id).first()
+        if not prod:
+            abort(404)
+        prod.name = name
+        prod.calories = calories
+        prod.proteins = proteins
+        prod.fats = fats
+        prod.carbs = carbs
+        prod.grams = grams
+        session.commit()
+
+    return {
+        'result': True
+    }
+
+
+@BP.post('/<int:product_id>/archive')
+@api_login_required_group(AccessGroup.Administrator)
+def archive(product_id: int):
+    with get_session() as session:
+        prod = session.query(Product).filter(Product.id == product_id).first()
+        if not prod:
+            abort(404)
+
+        prod.archived = True
+        session.commit()
+
+    return {
+        'result': True
+    }
--- app/tests/tests_api/test_products.py
+++ app/tests/tests_api/test_products.py
@@ -2,7 +2,9 @@ import pytest
 
 from flask.testing import FlaskClient
 
-from organizer.schema import Units
+from organizer.db import get_session
+from organizer.schema import Product, Units
+from organizer.strings import STRING_TABLE
 
 
 def test_api_search_rejects_not_logged_in(client: FlaskClient):
@@ -83,7 +85,7 @@ def test_api_units_fails_on_incorrect_id(org_logged_client: FlaskClient, id: str
     assert not result.json['result']
 
 
-def test_index_page_does_not_show_archived(org_logged_client: FlaskClient):
+def test_api_search_does_not_show_archived(org_logged_client: FlaskClient):
     result = org_logged_client.get('/api/products/search',
                                    query_string={'search': ''})
     assert result.status_code == 200
@@ -92,7 +94,7 @@ def test_index_page_does_not_show_archived(org_logged_client: FlaskClient):
         assert product['name'] != 'Archived'
 
 
-def test_index_page_change_pages(org_logged_client: FlaskClient):
+def test_api_search_change_pages(org_logged_client: FlaskClient):
     result = org_logged_client.get('/api/products/search',
                                     query_string={'page': 0})
     assert result.status_code == 200
@@ -108,7 +110,7 @@ def test_index_page_change_pages(org_logged_client: FlaskClient):
         assert product['name'] not in ['Multigrain cereal', 'Mango', 'Cream cheese']
 
 
-def test_index_page_searches(org_logged_client: FlaskClient):
+def test_api_search_searches(org_logged_client: FlaskClient):
     result = org_logged_client.get('/api/products/search',
                                     query_string={'search': 'jerk'})
     assert result.status_code == 200
@@ -116,3 +118,401 @@ def test_index_page_searches(org_logged_client: FlaskClient):
     for product in result.json['products']:
         assert product['name'] != 'Mango'
     assert any(map(lambda x: 'jerk' in x['name'], result.json['products']))
+
+
+def test_api_add_rejects_not_logged_in(client: FlaskClient):
+    result = client.post('/api/products/add')
+    assert result.status_code == 403
+
+
+def test_api_add_uses_correct_method(org_logged_client: FlaskClient):
+    result = org_logged_client.get('/api/products/add',
+                                   data={
+                                       'name': '',
+                                       'calories': '',
+                                       'proteins': '',
+                                       'fats': '',
+                                       'carbs': ''
+                                   })
+    assert result.status_code == 405
+
+
+def test_api_add_adds_product(org_logged_client: FlaskClient):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '21.2',
+                                        'proteins': '4.0',
+                                        'fats': '5.1',
+                                        'carbs': '1.1'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result'] == True
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').one()
+            assert prod.calories == 21.2
+            assert prod.proteins == 4.0
+            assert prod.fats == 5.1
+            assert prod.carbs == 1.1
+
+
+def test_api_add_adds_product_with_grams(org_logged_client: FlaskClient):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '21.2',
+                                        'proteins': '4.0',
+                                        'fats': '5.1',
+                                        'carbs': '1.1',
+                                        'grams': '14.4'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result'] == True
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').one()
+            assert prod.calories == 21.2
+            assert prod.proteins == 4.0
+            assert prod.fats == 5.1
+            assert prod.carbs == 1.1
+            assert prod.grams == 14.4
+
+
+def test_api_add_rejects_no_data(org_logged_client: FlaskClient):
+    result = org_logged_client.post('/api/products/add', json={})
+    assert result.status_code == 400
+
+
+@pytest.mark.parametrize('name', ['', 'a'*101])
+def test_api_add_rejects_incorrect_name(org_logged_client: FlaskClient, name: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': name,
+                                        'calories': '21.8',
+                                        'proteins': '4.9',
+                                        'fats': '5.6',
+                                        'carbs': '19.9'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect name'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(Product.name == name).first()
+            assert not prod
+
+
+@pytest.mark.parametrize('cals', ['-71.9', 'nan'])
+def test_api_add_rejects_incorrect_cals(org_logged_client: FlaskClient, cals: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': cals,
+                                        'proteins': '4.9',
+                                        'fats': '5.6',
+                                        'carbs': '19.9'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect calories'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+@pytest.mark.parametrize('prots', ['-71.9', 'nan', '171.9'])
+def test_api_add_rejects_incorrect_proteins(org_logged_client: FlaskClient, prots: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '120.0',
+                                        'proteins': prots,
+                                        'fats': '5.6',
+                                        'carbs': '19.9'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect proteins'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+@pytest.mark.parametrize('fats', ['-71.9', 'nan', '171.9'])
+def test_api_add_rejects_incorrect_fats(org_logged_client: FlaskClient, fats: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '120.0',
+                                        'proteins': '5.6',
+                                        'fats': fats,
+                                        'carbs': '19.9'
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect fats'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+@pytest.mark.parametrize('carbs', ['-71.9', 'nan', '171.9'])
+def test_api_add_rejects_incorrect_carbs(org_logged_client: FlaskClient, carbs: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '120.0',
+                                        'proteins': '5.6',
+                                        'fats': '19.9',
+                                        'carbs': carbs
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect carbs'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+@pytest.mark.parametrize('grams', ['-71.9', 'nan'])
+def test_api_add_rejects_incorrect_grams(org_logged_client: FlaskClient, grams: str):
+    result = org_logged_client.post('/api/products/add',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '120.0',
+                                        'proteins': '19.9',
+                                        'fats': '5.6',
+                                        'carbs': '17.1',
+                                        'grams': grams
+                                    })
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+    assert STRING_TABLE['Products error incorrect grams'] in result.json['error']
+
+    with org_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+def test_api_edit_rejects_not_logged_in(client: FlaskClient):
+    result = client.post('/api/products/1/edit',
+                         json={
+                             'name': 'Test product',
+                             'calories': '249.6',
+                             'proteins': '16.6',
+                             'fats': '71.9',
+                             'carbs': '41.9'
+                         })
+    assert result.status_code == 403
+
+
+def test_api_edit_rejects_users(org_logged_client: FlaskClient):
+    result = org_logged_client.post('/api/products/1/edit',
+                                    json={
+                                        'name': 'Test product',
+                                        'calories': '249.6',
+                                        'proteins': '16.6',
+                                        'fats': '71.9',
+                                        'carbs': '41.9'
+                                    })
+    assert result.status_code == 403
+
+
+def test_api_edit_uses_correct_method(org_logged_client: FlaskClient):
+    result = org_logged_client.get('/api/products/1/edit',
+                                   json={
+                                       'name': 'Test product',
+                                       'calories': '249.6',
+                                       'proteins': '16.6',
+                                       'fats': '71.9',
+                                       'carbs': '41.9'
+                                   })
+    assert result.status_code == 405
+
+
+def test_api_edit_redirects_after_edit(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/1/edit',
+                                      json={
+                                          'name': 'Test product',
+                                          'calories': '249.6',
+                                          'proteins': '16.6',
+                                          'fats': '71.9',
+                                          'carbs': '41.9'
+                                      })
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result']
+
+
+def test_api_edit_edits(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/1/edit',
+                                      json={
+                                          'name': 'Test product',
+                                          'calories': '249.6',
+                                          'proteins': '16.6',
+                                          'fats': '71.9',
+                                          'carbs': '41.9'
+                                      })
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result']
+
+    with admin_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').one()
+            assert prod.calories == 249.6
+            assert prod.proteins == 16.6
+            assert prod.fats == 71.9
+            assert prod.carbs == 41.9
+
+
+def test_api_edit_edits_with_grams(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/1/edit',
+                                      json={
+                                          'name': 'Test product',
+                                          'calories': '249.6',
+                                          'proteins': '16.6',
+                                          'fats': '71.9',
+                                          'carbs': '41.9',
+                                          'grams': '250.0'
+                                      })
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result']
+
+    with admin_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').one()
+            assert prod.calories == 249.6
+            assert prod.proteins == 16.6
+            assert prod.fats == 71.9
+            assert prod.carbs == 41.9
+            assert prod.grams == 250.0
+
+
+def test_api_edit_rejects_no_data(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/1/edit', json={})
+    assert result.status_code == 400
+
+
+@pytest.mark.parametrize('test_data', [
+    {'name': ''},
+    {'name': 'a'*101},
+    {'calories': '-71.9'},
+    {'calories': 'nan'},
+    {'proteins': '-71.9'},
+    {'proteins': 'nan'},
+    {'proteins': '171.9'},
+    {'fats': '-71.9'},
+    {'fats': 'nan'},
+    {'fats': '171.9'},
+    {'carbs': '-71.9'},
+    {'carbs': 'nan'},
+    {'carbs': '171.9'},
+    {'grams': '-71.9'},
+    {'grams': 'nan'},
+])
+def test_api_edit_rejects_incorrect_data(admin_logged_client: FlaskClient,
+                                         test_data: dict):
+    input_data = {
+        'name': 'Test product',
+        'calories': '249.6',
+        'proteins': '16.6',
+        'fats': '71.9',
+        'carbs': '41.9'
+    }
+    input_data.update(test_data)
+
+    result = admin_logged_client.post('/api/products/2/edit', json=input_data)
+
+    assert result.status_code == 200
+    assert result.json
+    assert not result.json['result']
+
+    with admin_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(Product.name == 'Mango').one()
+            assert prod.calories != 249.6
+            assert prod.proteins != 16.6
+            assert prod.fats != 71.9
+            assert prod.carbs != 41.9
+
+
+def test_api_edit_returns_404_for_non_existing_product(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/200/edit',
+                                      json={
+                                          'name': 'Test product',
+                                          'calories': '249.6',
+                                          'proteins': '16.6',
+                                          'fats': '71.9',
+                                          'carbs': '41.9'
+                                      })
+    assert result.status_code == 404
+    with admin_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(
+                Product.name == 'Test product').first()
+            assert not prod
+
+
+def test_api_archive_rejects_not_logged_in(client: FlaskClient):
+    result = client.post('/api/products/1/archive')
+    assert result.status_code == 403
+
+
+def test_api_archive_rejects_users(org_logged_client: FlaskClient):
+    result = org_logged_client.post('/api/products/1/archive')
+    assert result.status_code == 403
+
+
+def test_api_archive_uses_correct_method(admin_logged_client: FlaskClient):
+    result = admin_logged_client.get('/api/products/1/archive')
+    assert result.status_code == 405
+
+
+def test_api_archive_returns_404_for_non_existing_product(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/100500/archive')
+    assert result.status_code == 404
+
+
+def test_api_archive_archives_product(admin_logged_client: FlaskClient):
+    result = admin_logged_client.post('/api/products/2/archive')
+
+    assert result.status_code == 200
+    assert result.json
+    assert result.json['result']
+
+    with admin_logged_client.application.app_context():
+        with get_session() as session:
+            prod = session.query(Product).filter(Product.name == 'Mango').one()
+            assert prod.archived
