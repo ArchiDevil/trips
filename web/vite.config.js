39f97f343d19c6f2576575eee2b0daa5d5ce612e
Denis Bezykornov <archidevil52@gmail.com>
2023-05-01 18:38:06 +0300

    Add Vue and other deps to web package

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,16 +1,31 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import mockServer from 'vite-plugin-mock-server'
 
 // vite.config.js
 export default defineConfig({
-    build: {
-        assetsDir: 'static',
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html'),
-                info: resolve(__dirname, 'info.html'),
-                tutorial: resolve(__dirname, 'tutorial.html')
-            }
-        }
+  resolve: {
+    alias: {
+      vue: resolve('node_modules/vue/dist/vue.esm-bundler.js'),
+    },
+  },
+  build: {
+    assetsDir: 'static',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        info: resolve(__dirname, 'info.html'),
+        tutorial: resolve(__dirname, 'tutorial.html'),
+        products: resolve(__dirname, 'products.html'),
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    {
+      ...mockServer(),
+      apply: 'serve',
     }
+  ]
 })
