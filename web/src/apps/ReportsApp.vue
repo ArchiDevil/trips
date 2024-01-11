af44dfc9e7bde2bf73335b0844750b49040c0b64
Denis Bezykornov <archidevil52@gmail.com>
2024-01-12 02:33:56 +0300

    Add link handling

--- web/src/apps/ReportsApp.vue
+++ web/src/apps/ReportsApp.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
 import NavigationBar from '../components/NavigationBar.vue'
+import { useNavStore } from '../stores/nav'
+
+onMounted(() => {
+  useNavStore().link = 'trips'
+})
 </script>
 
 <template>
